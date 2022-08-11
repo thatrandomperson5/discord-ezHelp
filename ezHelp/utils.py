091522632e40de8fc1516e79bc8dba5b4070d67f
thatrandomperson5 <94306853+thatrandomperson5@users.noreply.github.com>
2022-08-10 20:06:51 -0500

    Create utils.py

---
+++ ezHelp/utils.py
@@ -0,0 +1,20 @@
+import discord
+
+def onmap(map):
+    out = []
+    for x, y in map.items():
+        out.append({"type": "cog", "value": x})
+        for z in y:
+            out.append({"type": "command", "value": x})
+    return out
+def oncog(cog):
+    all_commands = cog.get_commands()
+    all_commands.append(cog.qualified_name)
+    return all_commands
+def cogSplit(map):
+    """Makes cog mapping able to be paginated"""
+    
+    if isinstance(map, discord.ext.commands.Cog):
+        return oncog(map)
+    else:
+        return onmap(map)
